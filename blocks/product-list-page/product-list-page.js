b33b1347d3ef5ab4c62a0c6e2d5c9b83b0a255e4
Vinay Shah <vinshah@adobe.com>
2025-04-22 18:00:23 +0530

    aco config changes

--- blocks/product-list-page/product-list-page.js
+++ blocks/product-list-page/product-list-page.js
@@ -1,26 +1,37 @@
+/* eslint-disable import/no-unresolved */
+import { ProductListingPage } from '@dropins/storefront-search/containers/ProductListingPage.js';
+import { render as provider } from '@dropins/storefront-search/render.js';
 import { readBlockConfig } from '../../scripts/aem.js';
-import { getConfigValue } from '../../scripts/configs.js';
+import { getConfigValue, getHeaders } from '../../scripts/configs.js';
 import { rootLink } from '../../scripts/scripts.js';
 
-export default async function decorate(block) {
-  // eslint-disable-next-line import/no-absolute-path, import/no-unresolved
-  await import('/scripts/widgets/search.js');
+// Initializer
+import('../../scripts/initializers/search.js');
 
-  const { category, urlpath, type } = readBlockConfig(block);
+export default async function decorate(block) {
+  const { urlpath, category, type } = readBlockConfig(block);
   block.textContent = '';
 
-  const storeDetails = {
-    environmentId: getConfigValue('headers.cs.Magento-Environment-Id'),
-    environmentType: (getConfigValue('commerce-endpoint')).includes('sandbox') ? 'testing' : '',
-    apiKey: getConfigValue('headers.cs.x-api-key'),
+  // Store Config
+  const storeConfig = {
+    type: 'eds',
+    environmentType: getConfigValue('analytics.environment').includes('Production') ? '' : 'testing',
     apiUrl: getConfigValue('commerce-endpoint'),
-    websiteCode: getConfigValue('headers.cs.Magento-Website-Code'),
-    storeCode: getConfigValue('headers.cs.Magento-Store-Code'),
-    storeViewCode: getConfigValue('headers.cs.Magento-Store-View-Code'),
+    customerGroup: getConfigValue('headers.cs.Magento-Customer-Group'),
+    route: ({ sku, urlKey }) => {
+      const a = new URL(window.location.origin);
+      a.pathname = rootLink(`/products/${urlKey}/${sku}`);
+      return a.toString();
+    },
+    defaultHeaders: {
+      'Content-Type': 'application/json',
+      ...getHeaders('cs'),
+    },
+    // PLP config
     config: {
       pageSize: 8,
       perPageConfig: {
-        pageSizeOptions: '12,24,36',
+        pageSizeOptions: '12, 24, 36',
         defaultPageSizeOption: '12',
       },
       minQueryLength: '2',
@@ -31,7 +42,8 @@ export default async function decorate(block) {
       imageCarousel: false,
       optimizeImages: true,
       imageBaseWidth: 200,
-      listview: true,
+      listview: false,
+      currentCategoryUrlPath: null,
       displayMode: '', // "" for plp || "PAGE" for category/catalog
       addToCart: async (...args) => {
         const { addProductsToCart } = await import('../../scripts/__dropins__/storefront-cart/api.js');
@@ -41,34 +53,34 @@ export default async function decorate(block) {
           quantity: args[2],
         }]);
       },
-    },
-    context: {
-      customerGroup: getConfigValue('headers.cs.Magento-Customer-Group'),
-    },
-    route: ({ sku, urlKey }) => {
-      const a = new URL(window.location.origin);
-      a.pathname = rootLink(`/products/${urlKey}/${sku}`);
-      return a.toString();
+      route: {
+        route: rootLink('/search'),
+        query: 'q',
+      },
     },
   };
 
+  // for non search pages
   if (type !== 'search') {
-    storeDetails.config.categoryName = document.querySelector('.default-content-wrapper > h1')?.innerText;
-    storeDetails.config.currentCategoryId = category;
-    storeDetails.config.currentCategoryUrlPath = urlpath;
+    storeConfig.config.categoryName = document.querySelector('.default-content-wrapper > h1')?.innerText;
+    storeConfig.config.currentCategoryId = category;
+    storeConfig.config.currentCategoryUrlPath = urlpath;
 
     // Enable enrichment
     block.dataset.category = category;
   }
 
-  await new Promise((resolve) => {
-    const interval = setInterval(() => {
-      if (window.LiveSearchPLP) {
-        clearInterval(interval);
-        resolve();
-      }
-    }, 200);
-  });
+  const widget = document.createElement('div');
+  widget.id = 'plp-widget-container';
+  block.appendChild(widget);
 
-  return window.LiveSearchPLP({ storeDetails, root: block });
+  const rootElement = document.getElementById('plp-widget-container');
+
+  if (rootElement) {
+    provider.render(ProductListingPage, { storeConfig })(
+      rootElement,
+    );
+  } else {
+    console.warn('Root element #plp-widget-container not found.');
+  }
 }
